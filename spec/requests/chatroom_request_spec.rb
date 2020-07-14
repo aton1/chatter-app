8c89a6ecde04eb7a14a777e9395f2decbe77391d
aton1 <aton@peerstreet.com>
2020-07-13 18:35:48 -0700

    refactored chatbox messages into a partial

--- spec/requests/chatroom_request_spec.rb
+++ spec/requests/chatroom_request_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Chatrooms", type: :request do
+  let(:messages) { create_list(:message, 4) }
+  
   it "displays chatroom as root path" do
     get root_path
 
     expect(response).to be_successful
+  end
+
+  it "displays messages" do 
+    messages.each do |message|
+      get root_path
+
+      expect(response.body).to include(message.body)
+    end
   end
 end
